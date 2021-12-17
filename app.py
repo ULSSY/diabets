881aba6b0b5ac8efd203897c8e8aed355feee298
ULSSY <96038813+ULSSY@users.noreply.github.com>
2021-12-17 17:05:15 +0900

    2021-12-17

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import joblib
 from eda_app import run_eda_app
-#from ml_app import run_ml_app
+from ml_app import run_ml_app
 
 def main():
     menu=['홈','데이터분석','인공지능']
@@ -14,11 +14,11 @@ def main():
 
     if choice=='홈':
         st.subheader('당뇨병 데이터 분석 및 예측')
-    elif choice =='데이터 분석':
-      #  run_eda_app()
-        pass
+    elif choice =='데이터분석':
+        run_eda_app()
+        
     elif choice =='인공지능':
-      #  run_ml_app()
-        pass
+        run_ml_app()
+      
 if __name__ == '__main__':
     main()
